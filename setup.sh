4bd5c5596afb105f48a1c94e94283876eda7f7e7
ncoudsi <nicolas.coudsi@gmail.com>
2021-01-26 15:06:10 +0100

    several fixes, nginx, pma and wordpress containers SEEM healthy [UNSTABLE]

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 #Build and run the Nginx container.
-sudo docker build ./srcs/nginx -t nginx > logs/nginx_logs.log
+sudo docker build ./srcs/nginx -t nginx > logs/nginx_build_logs.log
 sudo docker run -dt nginx
 
 #Build and run the Wordpress container.
-sudo docker build ./srcs/wordpress -t wordpress > logs/wordpress_logs.log
+sudo docker build ./srcs/wordpress -t wordpress > logs/wordpress_build_logs.log
 sudo docker run -dt wordpress
 
 #Build and run the phpMyAdmin container.
-sudo docker build ./srcs/phpmyadmin -t phpmyadmin > logs/phpmyadmin_logs.log
+sudo docker build ./srcs/phpmyadmin -t phpmyadmin > logs/phpmyadmin_build_logs.log
 sudo docker run -dt phpmyadmin
 
 #Build and run the MySQL container.
-sudo docker build ./srcs/mysql -t mysql > logs/mysql_logs.log
+sudo docker build ./srcs/mysql -t mysql > logs/mysql_build_logs.log
 sudo docker run -dt mysql
